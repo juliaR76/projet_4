593c887cbcc57897c99ad8b22ab5ccb88a8ae6ec
julia.R <julia.ristic@gmail.com>
2019-03-26 20:05:06 +0100

    ajout du code HTML dans le fichier update

--- update.php
+++ update.php
@@ -31,25 +31,59 @@ $req->execute([
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>UPDATE</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Billet pour l'Alaska</title>
 </head>
 <body>
-    <header>
-    <h1>Billet simple pour l'Alaska</h1>
+    <header class="masthead" style="background-image: url('img/Anchorage_3.jpg')">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <div class="site-heading">
+                <h1>Billet pour l'Alaska</h1>
+                <span class="subheading">Modifier votre billet</span>
+              </div>
+            </div>
+          </div>
+        </div>
     </header>
 
 <?php while($donnee = $req->fetch()){ ?>
-    <div>
-        <form action="update.php?billet=<?= $_GET['billet'] ?>" method="post">
-            <p><input type="text" name="titre" placeholder="titre" value="<?= htmlspecialchars($donnee['titre']) ?>"></p>
-            <P><textarea name="contenu" id="" cols="30" rows="10"><?= htmlspecialchars($donnee['contenu'])?></textarea></p>
-            <p>De <?= htmlspecialchars($donnee['auteur']) ?>, le <?= htmlspecialchars($donnee['date_ajout_fr']) ?> </p>
-            <button type="submit">Enregistrer</button>
-        </form>       
+    <div class="container">
+        <a href="creation.php">Retour à l'Admin</a>
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <form action="update.php?billet=<?= $_GET['billet'] ?>" method="post">
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Auteur</label>
+                        <input type="text" class="form-control" name="auteur" placeholder="Auteur" value="<?= htmlspecialchars($donnee['auteur']) ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Titre</label>
+                        <input type="text" class="form-control" name="titre" placeholder="Titre" value="<?= htmlspecialchars($donnee['titre']) ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Corps de texte</label>
+                        <textarea class="form-control" name="contenu" rows="10"><?= htmlspecialchars($donnee['contenu'])?></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-dark">Modifier</button>
+                    <button type="button" class="btn btn-dark"><a href="creation.php">Annuler</a></button>
+                </form>
+            </div>  
+        </div>
     </div>
+    
 <?php } ?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>   
+
 </body>
 </html>
 
