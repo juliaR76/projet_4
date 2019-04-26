add32766f21a87dd9f9509649a2f711360747b67
julia.R <julia.ristic@gmail.com>
2019-04-26 17:29:11 +0200

    mise à jour du fichier index.php en POO

--- index.php
+++ index.php
@@ -1,102 +1,74 @@
 <?php
-try
-{
-    //connexion base de donnee projet_4
-$bdd = new PDO('mysql:host=localhost; dbname=projet_4;charset=utf8', 'root', '');
-}
-catch (exception $e)
-{
-    die('Erreur : '.$e->getMessage());
-}
 
-//recuperer les donnees de la table
-$req = $bdd->prepare('SELECT id, titre, auteur, contenu, DATE_FORMAT(date_ajout, \'%d/%m/%Y à %Hh%imin\') 
-AS date_ajout_fr
-FROM billet');
-$req->execute();
+require("poo/Billet.php");
+require("poo/BilletManager.php");
+require("poo/Commentaire.php");
+require("poo/CommentaireManager.php");
+
+// recupere les billets
+
+$billetManager = new BilletManager; 
+$billet = $billetManager->getDate();
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Billet pour l'Alaska</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Billet simple pour l'Alaska</title>
 </head>
 <body>
-   <!-- Navigation -->
-   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div class="container">
-          <a class="navbar-brand" href="index.html">Jean Fastoche</a>
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Accueil</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="post.php">Articles</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="connexion.php">Connexion</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
 
-      <!-- Page Header -->
-      <header class="masthead" style="background-image: url('img/Anchorage_3.jpg')">
-        <div class="overlay"></div>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-              <div class="site-heading">
-                <h1>Billet pour l'Alaska</h1>
-                <span class="subheading">A Blog Theme by Start Bootstrap</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
-    
-      <!-- Main Content -->
-      
-<?php while($donnee = $req->fetch()){ ?>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <div class="post-preview">
-              <a href="post.php?billet=<?= $billet['id'] ?>">
-                <h2 class="post-title"><strong><?= htmlspecialchars($billet['titre']) ?></strong></h2>
-              </a>
-              <p><?= htmlspecialchars($billet['contenu'])?></p>
-              <p class="post-meta">Posted by <?= htmlspecialchars($billet['auteur']) ?>, le <?= htmlspecialchars($billet['date_ajout_fr']) ?></p>
-              <i class="far fa-comment-alt"></i><a href="post.php?billet=<?= $billet['id'] ?>"> commentaire</a>
-            <hr>
-            </div>
+<?php include("view/menu.php")?>
+ 
+<!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/Anchorage_3.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>Billet simple pour l'Alaska</h1>
           </div>
         </div>
       </div>
-<?php } ?>
+    </div>
+  </header>
+ 
+<!-- Main Content -->
 
-      <div class="clearfix">
-        <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="post-preview">
+          <a href="post.php?billet=<?= $billet->id() ?>">
+            <h2 class="post-title"><strong><?= htmlspecialchars($billet->titre()) ?></strong></h2>
+          </a>
+          <p><?= htmlspecialchars($billet->contenu())?></p>
+          <p class="post-meta">Posted by <?= htmlspecialchars($billet->auteur()) ?>, le <?= htmlspecialchars($billet->date_ajout()) ?></p>
+          <i class="far fa-comment-alt"></i><a href="post.php?billet=<?= $billet->id() ?>"> commentaires</a>
+          <hr>
+        </div>
       </div>
-     
-    
-      <!-- Bootstrap core JavaScript -->
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>   
+    </div>
+  </div>
+
+  <div class="clearfix">
+    <a class="btn btn-dark float-right" href="billets.php">Plus d'articles &rarr;</a>
+  </div>
+
+ 
+<!-- Bootstrap core JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
 </body>
 </html>
