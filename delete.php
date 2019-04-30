ec7ccd0598b86f365edd9bab71a4141342e78719
julia.R <julia.ristic@gmail.com>
2019-04-30 18:48:40 +0200

    mise à jour du fichier delete en POO + mise en page bootstrap

--- delete.php
+++ delete.php
@@ -1,25 +1,94 @@
 <?php
-var_dump($_GET['billet']);
-try
-{
-    //connexion base de donnee projet_4
-$bdd = new PDO('mysql:host=localhost; dbname=projet_4;charset=utf8', 'root', '');
-}
-catch (exception $e)
-{
-    die('Erreur : '.$e->getMessage());
-}
 
-//recupere les donnees de la table
-$req = $bdd->prepare('SELECT id, titre, auteur, contenu, DATE_FORMAT(date_ajout, \'%d/%m/%Y à %Hh%imin\') 
-AS date_ajout_fr FROM billet');
-$req->execute();
+require("poo/Billet.php");
+require("poo/Commentaire.php");
+
+require("poo/BilletManager.php");
+require("poo/CommentaireManager.php");
 
 
-//supprimer la donnes en rapport avec l'id
-$req = $bdd->prepare('DELETE FROM billet WHERE id = :id');
-$req->execute([
-    "id" => $_GET['billet'],
-]);
+//recupere les donnees de la table billet
+
+$billetManager = new BilletManager;
+$billet = $billetManager->getList();
+
+
+//supprimer le billet en rapport avec l'id
+
+$billet = new BilletManager;
+$billet = $billetManager->delete($_GET['billet']);
+
+
+//recuper les donnees de table commentaire
+
+$commentaireManager = new CommentaireManager;
+$commentaires = $commentaireManager->get($_GET['billet']);
+
+//supprimer le commentaire en rapport avec l'id
+
+$commentaireManager = new CommentaireManager;
+$commentaires = $commentaireManager->delete($_GET['billet']);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Billet simple pour l'Alaska</title>
+</head>
+<body>
+<!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Jean Forteroche</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+             Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Retour au site</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="creation.php">Admin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="connexion.php">Connexion</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="masthead" style="background-image: url('img/Anchorage_3.jpg')">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <div class="site-heading">
+                        <h1>Billet simple pour l'Alaska</h1>
+                        <span class="subheading">Admin</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+        <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">HEY !</h4>
+            <p>Ce billet à bien été supprimé ! </p>
+            <hr>
+            <p class="mb-0"><a href="creation.php">Retour à l'admin</a></p>
+        </div>
+
+<!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>   
+</body>
+</html>
